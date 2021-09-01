95221055cc5f7b37c97915a8c7d8cda38aa58bc6
sobbaka <a.bagaev1989@gmail.com>
2021-09-01 10:09:33 +0300

    Added api for Enter the chat for authenticated users

--- chat/api/urls.py
+++ chat/api/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from .views import (
     api_get_chat_history,
     api_sent_message,
-    api_create_chat
+    api_create_chat,
+    api_enter_the_chat
 )
 
 app_name = 'chat'
 
 urlpatterns = [
+    path('enter_chat/', api_enter_the_chat, name="api_enter_the_chat"),
     path('create_chat/', api_create_chat, name="api_create_chat"),
     path('create/', api_sent_message, name="sent_message"),
     path('<pk>/', api_get_chat_history, name="detail"),
--- chat/api/views.py
+++ chat/api/views.py
@@ -1,16 +1,17 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
-from accounts.models import CustomUser
+from rest_framework.authtoken.models import Token
+
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+
 from chat.models import Message, ChatRoom
 from .serializers import MessageSerializer, ChatRoomSerializer
+# from accounts.models import CustomUser
 
-SUCCESS = 'success'
-ERROR = 'error'
-DELETE_SUCCESS = 'deleted'
-UPDATE_SUCCESS = 'updated'
-CREATE_SUCCESS = 'created'
 
 @api_view(['GET', ])
 def api_get_chat_history(request, pk):
@@ -50,3 +51,42 @@ def api_create_chat(request):
 			serializer.save()
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def sent_message(chat_id, user):
+	channel_layer = get_channel_layer()
+	chat = f'chat_{chat_id}'
+	message = {
+		'type': 'info_message',
+		'username': user.username,
+		'user_id': user.id,
+		'flag': 'NEW'
+	}
+	async_to_sync(channel_layer.group_send)(chat, message)
+
+
+@api_view(['POST',])
+@permission_classes([IsAuthenticated])
+def api_enter_the_chat(request):
+	token = request.headers['Authorization'].strip('Token ')
+
+	try:
+		chat_id = request.POST.get('chat_id')
+		chat = ChatRoom.objects.get(id=chat_id)
+	except ChatRoom.DoesNotExist:
+		return Response(status=status.HTTP_404_NOT_FOUND)
+
+	try:
+		user = Token.objects.get(key=token).user
+	except ChatRoom.DoesNotExist:
+		return Response(status=status.HTTP_404_NOT_FOUND)
+
+	if request.method == 'POST':
+		try:
+			chat.users.add(user)
+			sent_message(chat_id, user)
+			data = {}
+			data['SUCCESS'] = 'UPDATE_SUCCESS'
+		except ChatRoom.DoesNotExist:
+			return Response(status=status.HTTP_404_NOT_FOUND)
+		return Response(data=data)
